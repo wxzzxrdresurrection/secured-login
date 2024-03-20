cc3167db5b1d2a4088a27dbf7fee9dd8d497fbd8
wxzzxrdresurrection <magicwizz12@gmail.com>
2024-03-20 17:10:57 -0600

    styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
-<body>
+<body style="background-color: white">
     @if ($role ==  1 )
-    Vista de administrador
+    <h1>Vista de administrador</h1>
     @endif
     @if ($role ==  2 )
-    Vista de usuario
+    <h1>Vista de usuario</h1>
     @endif
     <a href="{{route('logout')}}"><button>Logout</button></a>
 </body>
