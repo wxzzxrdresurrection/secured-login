6a031bc38d8f0ee27be849dfd6b56c30ebc31810
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-04-04 19:53:51 -0600

    styles

--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Pacientes</title>
 </head>
 <body class="bg-sky-200">
