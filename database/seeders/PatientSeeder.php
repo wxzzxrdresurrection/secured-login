a03fc034af48766bd49da5371b10fbafbd8bf659
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-04-07 22:28:54 -0600

    seeder fix

--- database/seeders/PatientSeeder.php
+++ database/seeders/PatientSeeder.php
@@ -17,7 +17,6 @@ class PatientSeeder extends Seeder
             [
                 "name" => "Josue",
                 "last_name" => "Soto",
-                "birth_date" => "2006-11-15",
                 "gender" => "M",
                 "insurance_id" => 1
             ],
@@ -27,7 +26,6 @@ class PatientSeeder extends Seeder
             [
                 "name" => "Carlos",
                 "last_name" => "Reyes",
-                "birth_date" => "2003-02-14",
                 "gender" => "M",
                 "insurance_id" => 3
             ]
